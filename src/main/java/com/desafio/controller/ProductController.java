596035d451e199d88e26d203f7e82c4e71621f45
Marcos Barbosa <marcos_pp.16@hotmail.com>
2022-03-09 16:57:54 -0300

    Implementação da camada service

--- src/main/java/com/desafio/controller/ProductController.java
+++ src/main/java/com/desafio/controller/ProductController.java
@@ -1,7 +1,9 @@
 package com.desafio.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,12 +12,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.desafio.domain.model.Product;
+import com.desafio.service.ProductService;
 
 @RestController
 @RequestMapping("/api/products")
 public class ProductController {
+	@Autowired
+	private ProductService productService;
 	@PostMapping()
-	public ResponseEntity<List<Product>> save(@RequestBody List<Product> product) {
-		return ResponseEntity.status(HttpStatus.CREATED).body(product);
+	public ResponseEntity<ArrayList<Product>> save(@RequestBody List<Product> product) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(productService.save(product));
 	}
 }
---
+++ src/main/java/com/desafio/repository/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.desafio.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.desafio.domain.model.Product;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
