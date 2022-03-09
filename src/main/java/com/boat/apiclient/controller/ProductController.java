2f7ff65bcf207e4d9a7478de98ab1e38d76d4966
Marcos Barbosa <marcos_pp.16@hotmail.com>
2022-03-09 13:46:13 -0300

    Primeiro commit do desafio

---
+++ src/main/java/com/boat/apiclient/controller/ProductController.java
@@ -0,0 +1,21 @@
+package com.boat.apiclient.controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.boat.apiclient.domain.model.Product;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+	@PostMapping()
+	public ResponseEntity<List<Product>> save(@RequestBody List<Product> product) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(product);
+	}
+}
---
+++ src/test/java/com/boat/apiclient/DesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boat.apiclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
